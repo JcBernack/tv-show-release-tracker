4c6ae8edc369a875f2326cd7c21139e08c80609a
Jan Christoph Bernack <jc.bernack@gmail.com>
2018-12-14 04:54:47 +0100

    async fetch

--- index.js
+++ index.js
@@ -5,81 +5,79 @@ const URL = require("url").URL;
 const fetch = require('node-fetch');
 
 function getUrl(path, queryParams) {
-	const url = new URL(process.env.API_URL);
-	url.pathname = "/" + process.env.API_VERSION + path;
-	url.searchParams.append("api_key", process.env.API_KEY);
-	if (queryParams) {
-		Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
-	}
-	return url;
+    const url = new URL(process.env.API_URL);
+    url.pathname = "/" + process.env.API_VERSION + path;
+    url.searchParams.append("api_key", process.env.API_KEY);
+    if (queryParams) {
+        Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
+    }
+    return url;
 }
 
 async function apiSeriesSearch(query) {
-	const url = getUrl("/search/tv", { query: query });
-	const response = await fetch(url);
-	return response.json();
+    const url = getUrl("/search/tv", {query});
+    const response = await fetch(url);
+    return response.json();
 }
 
 async function apiSeriesInfo(id) {
-	const url = getUrl("/tv/" + id);
-	const response = await fetch(url);
-	return response.json();
-}
-
-async function getItemId(item) {
-	// use id if given
-	if (item.id) return item.id;
-	// otherwise search using name
-	const searchResult = await apiSeriesSearch(item.name);
-	return searchResult.results[0].id;
+    const url = getUrl("/tv/" + id);
+    const response = await fetch(url);
+    return response.json();
 }
 
 function formatEpisodeNumber(season, episode) {
-	const s = season.toString().padStart(2, "0");
-	const e = episode.toString().padStart(2, "0");
-	return `s${s}e${e}`;
+    const s = season.toString().padStart(2, "0");
+    const e = episode.toString().padStart(2, "0");
+    return `s${s}e${e}`;
 }
 
 function formatDate(air_date) {
-	return new Date(air_date).toDateString();
+    return new Date(air_date).toDateString();
 }
 
-async function handleItem(item) {
-	try {
-		// get series id
-		const id = await getItemId(item);
-		// get series
-		const data = await apiSeriesInfo(id);
-		// output dates
-		console.log(`${data.name} - ${id}`);
-		if (item.season && item.season >= data.number_of_seasons) {
-			console.log("=> nothing new");
-		} else {
-			const last = data.last_episode_to_air;
-			if (last) {
-				const last_nr = formatEpisodeNumber(last.season_number, last.episode_number);
-				console.log(`  - last epsiode to air: ${last_nr} on ${formatDate(last.air_date)}`);
-			}
-			const next = data.next_episode_to_air;
-			if (next) {
-				const next_nr = formatEpisodeNumber(next.season_number, next.episode_number);
-				console.log(`  - next episode to air: ${next_nr} on ${formatDate(next.air_date)}`);
-			}
-		}
-		console.log("");
-	} catch(ex) {
-		console.warn("error", ex);
-	}
-}
-
-async function main(items) {
-	for (let i = 0; i < items.length; i++) {
-		// process items synchronously to not mess up the order of the items
-		//TODO: process asynchronously while keeping the order
-		await handleItem(items[i]);
-	}
+async function main(shows) {
+    // fetch all shows
+    const results = await Promise.all(shows.map(async show => {
+        let data = null;
+        try {
+            // search using name when no id given
+            if (!show.id) {
+                const searchResult = await apiSeriesSearch(show.name);
+                show.id = searchResult.results[0].id;
+            }
+            // get info on the show
+            data = await apiSeriesInfo(show.id);
+        } catch (ex) {
+            console.warn(ex);
+        }
+        return {show, data};
+    }));
+    // print results
+    results.forEach(({show, data}) => {
+        if (!data) {
+            console.warn("unable to fetch data for", show);
+            return;
+        }
+        console.log(`${data.name} - ${data.id}`);
+        if (show.season && show.season >= data.number_of_seasons) {
+            console.log("=> nothing new");
+        } else {
+            const last = data.last_episode_to_air;
+            if (last) {
+                const last_nr = formatEpisodeNumber(last.season_number, last.episode_number);
+                console.log(`  - last epsiode to air: ${last_nr} on ${formatDate(last.air_date)}`);
+            }
+            const next = data.next_episode_to_air;
+            if (next) {
+                const next_nr = formatEpisodeNumber(next.season_number, next.episode_number);
+                console.log(`  - next episode to air: ${next_nr} on ${formatDate(next.air_date)}`);
+            }
+        }
+        console.log("");
+    });
 }
 
 // process state.json
 const state = require("./state.json");
-main(state.items);
+main(state.shows).then(() => console.log("done"));

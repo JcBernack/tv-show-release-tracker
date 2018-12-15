5dd13353e1f66b4b2d5466b365cd3249f8a45cae
Jan Christoph Bernack <jc.bernack@gmail.com>
2018-12-15 23:17:13 +0100

    parse command line arguments

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 // API documentation https://developers.themoviedb.org/3/getting-started/introduction
 
 require("dotenv").config();
+const commander = require('commander');
 const URL = require("url").URL;
 const fetch = require('node-fetch');
 const columnify = require("columnify");
+
+// define command line interface
+commander
+    .version("0.1.0")
+    .option("-f, --file", "json file to process, defaults to ./state.json")
+    .option("-a, --all", "shows without new content will be hidden without this flag")
+    .parse(process.argv);
 
 function buildUrl(path, queryParams) {
     const url = new URL(process.env.API_URL);
     url.pathname = "/" + process.env.API_VERSION + path;
     url.searchParams.append("api_key", process.env.API_KEY);
-    if (queryParams) {
-        Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
-    }
+    if (queryParams) Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
     return url;
 }
 
+//TODO: handle rate limiting here https://developers.themoviedb.org/3/getting-started/request-rate-limiting
 const api = async (path, params) => (await fetch(buildUrl(path, params))).json();
 const apiSeriesSearch = (query) => api("/search/tv", {query});
 const apiSeriesInfo = id => api("/tv/" + id);
@@ -29,7 +36,7 @@ function formatDate(air_date) {
     return new Date(air_date).toDateString();
 }
 
-async function main(shows) {
+async function printShowsTable(shows) {
     // fetch all shows
     const results = await Promise.all(shows.map(async show => {
         let data = null;
@@ -42,22 +49,22 @@ async function main(shows) {
             // get info on the show
             data = await apiSeriesInfo(show.id);
         } catch (ex) {
+            console.warn("unable to fetch data for", show);
             console.warn(ex);
+            console.warn();
         }
         return {show, data};
     }));
     // generate result data
     const columns = results
-        .filter(({show, data}) => {
-            if (!data) console.warn("unable to fetch data for", show);
-            return data;
-        })
+        .filter(({data}) => data)
+        .filter(({show, data}) => commander.all || show.season < data.number_of_seasons)
         .map(({show, data}) => {
             const output = {
                 id: data.id,
                 name: data.name,
                 status: data.status,
-                seasons: `${show.season ? show.season : "*"}/${data.number_of_seasons}`
+                seasons: `${show.season ? show.season : "*"}/${data.number_of_seasons}`,
             };
             const last = data.last_episode_to_air;
             if (last) {
@@ -84,6 +91,7 @@ async function main(shows) {
     }));
 }
 
-// process state.json
-const state = require("./state.json");
-main(state.shows);
+// read given file
+const state = require(commander.file || "./state.json");
+// process contained shows
+printShowsTable(state.shows);
